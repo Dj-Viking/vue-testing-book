0dbbb8ddc455401f6bd58057758c9de683650753
DJ Viking <anders.swedishviking@gmail.com>
2021-01-21 05:29:06 -0500

    chapter 2 vue-hackernews testing Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <ItemList />
+</template>
+
+<script>
+import ItemList from './views/ItemList.vue';
+
+export default {
+  components: {
+    ItemList
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,24 @@
+// vue.config.js
+module.exports = {
+  lintOnSave: false,
+  // publicPath: 
+  //   process.env.NODE_ENV === 'production' ? '/v3/quote-builder/' : '',
+  filenameHashing: false,
+  chainWebpack: config => {
+    if(config.plugins.has('extract-css')) {
+      const extractCSSPlugin = config.plugin('extract-css')
+      extractCSSPlugin && extractCSSPlugin.tap(() => [
+        {
+          filename: '[name].css',
+          chunkFilename: '[name].css'
+        }
+      ])
+    }
+  },
+  configureWebpack: {
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js'
+    }
+  }
+};
