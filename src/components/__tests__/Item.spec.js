ce952f7b59619bc48a4f07281102d78be4550f25
DJ Viking <anders.swedishviking@gmail.com>
2021-01-21 07:23:07 -0500

    Check if textContent is contained within the vue component

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -7,6 +7,25 @@ import { test, describe, expect } from '@jest/globals';
 describe('Item.vue', () => {
   test('sanity test', () => {
     expect(true).toBe(true);
-    console.log(Item);
   });
+
+  test('renders "item"', () => {
+    // to mount a single file component
+    // need to convert copmonent options into a vue constructor
+    // creating a Vue constructor with the Vue.extend() method
+    const Ctor = Vue.extend(Item);
+
+    // create a vue instance
+    const vm = new Ctor().$mount();
+    console.log(vm.$el.textContent);
+
+    // access the HTML content
+    // toContain looks for substring so it can include 
+    // some of the string but not all of it
+    expect(vm.$el.textContent).toContain('item');
+    // toBe is strict equal
+    expect(vm.$el.textContent).toBe('\n  item\n');
+  });
+
+  
 });
